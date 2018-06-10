c4403ee7bd0da74f445e07cd6754928eb02203d3
Jousen-PC\Jousen <Romero87_2112@hotmail.com>
2018-06-10 02:46:14 +0200

    Override

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+	public void MyStartHost(){
+		StartHost();
+		Debug.Log (Time.timeSinceLevelLoad);
+	}
+
+	public override void OnStartHost ()
+	{
+		Debug.Log (Time.timeSinceLevelLoad);
+	}
+}
